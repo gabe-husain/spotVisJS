fe2df37fb4f2259b03a1e34e95474916c9457689
gabe-husain <gabe.husain@gmail.com>
2022-06-14 02:38:53 -0400

    added js

--- login.js
+++ login.js
@@ -1,1 +1,6 @@
-window.location.href = "http://www.w3schools.com";
+callbackURI = "gabe-husain.github.io/callback.html"
+scope = "user-read-email user-top-read playlist-read-private playlist-modify-private playlist-modify-public"
+window.location.href = `https://accounts.spotify.com/authorize?
+    response_type=code&client_id={secret.CLIENT_ID}&
+    redirect_uri={encodeURIComponent(callbackURI)}&
+    scope={encodeURIComponent(scope)}`;
