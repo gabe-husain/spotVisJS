992a0e307a973424700015c6d18d9e8c6865bf72
gabe-husain <gabe.husain@gmail.com>
2022-06-14 02:53:21 -0400

    removed more new lines

--- login.js
+++ login.js
@@ -1,6 +1,4 @@
 callbackURI = "gabe-husain.github.io/callback.html"
 scope = "user-read-email user-top-read playlist-read-private playlist-modify-private playlist-modify-public"
-window.location.href = `https://accounts.spotify.com/authorize?
-response_type=code&client_id=${secret.CLIENT_ID}&
-redirect_uri=${encodeURIComponent(callbackURI)}&
-scope=${encodeURIComponent(scope)}`;
+
+window.location.href = `https://accounts.spotify.com/authorize?response_type=code&client_id=${secret.CLIENT_ID}&redirect_uri=${encodeURIComponent(callbackURI)}&scope=${encodeURIComponent(scope)}`;
