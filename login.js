b097dc83c7d89202606403ac9906d777a80eace5
gabe-husain <gabe.husain@gmail.com>
2022-06-14 03:03:31 -0400

    added client id

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
 callbackURI = "gabe-husain.github.io/callback.html";
 scope = "user-read-email user-top-read playlist-read-private playlist-modify-private playlist-modify-public";
-
-window.location.href = `https://accounts.spotify.com/authorize?response_type=code&client_id=${{secret.CLIENT_ID}}&redirect_uri=${{encodeURIComponent(callbackURI)}}&scope=${{encodeURIComponent(scope)}}`;
+CLIENT_ID = "97c5540177094023a67cccb578e90612"
+window.location.href = `https://accounts.spotify.com/authorize?response_type=code&client_id=${ CLIENT_ID }&redirect_uri=${ encodeURIComponent(callbackURI) }&scope=${{encodeURIComponent(scope)}}`;
