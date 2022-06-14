22c3cb9730d76fd0e9c11abda9fcbb64f8344c1b
gabe-husain <gabe.husain@gmail.com>
2022-06-14 02:46:04 -0400

    removed whitespace

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 callbackURI = "gabe-husain.github.io/callback.html"
 scope = "user-read-email user-top-read playlist-read-private playlist-modify-private playlist-modify-public"
 window.location.href = `https://accounts.spotify.com/authorize?
-    response_type=code&client_id=${secret.CLIENT_ID}&
-    redirect_uri=${encodeURIComponent(callbackURI)}&
-    scope=${encodeURIComponent(scope)}`;
+response_type=code&client_id=${secret.CLIENT_ID}&
+redirect_uri=${encodeURIComponent(callbackURI)}&
+scope=${encodeURIComponent(scope)}`;
